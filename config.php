72bcdcb2a1bcc4e61fe8742b11883a92b75d0209
davey stone but bfdi mouth <118952873+davidsosa2022@users.noreply.github.com>
2024-03-09 21:10:25 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-const CONFIG_DB_SERVER = 'localhost';
-const CONFIG_DB_USER = 'tvii';
-const CONFIG_DB_PASS = 'O8tcx*DAzGc7]*Td';
-const CONFIG_DB_NAME = 'tvii';
+const CONFIG_DB_SERVER = '';
+const CONFIG_DB_USER = '';
+const CONFIG_DB_PASS = '';
+const CONFIG_DB_NAME = '';
 
 const CONFIG_CDN_FOLDER = '/img/cdn';
 const CONFIG_OLV_SSL_CERT = '/var/www/tvii/cert/cacert.pem';
